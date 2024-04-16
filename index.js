56ca23564be0269466d85eed95288e5d62afbf4c
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-04-16 18:24:15 +0530

    initial

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const http = require('http')
 const express = require('express')
 
+
 const app = express()
 const path = require('path');
+const { Socket } = require('dgram');
 const server = http.createServer(app);
 
 
+const { Server } = require("socket.io");
+const io = new Server(server)
+
 app.use(express.static(path.resolve("./public")));
+
+//socket.io
+io.on('connection', (socket) =>{
+    console.log("A new user has connected", socket.id)
+})
 
 
 
