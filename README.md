ddeec66a8dd0fee475bf903f7587fbaef166b3bf
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-04-16 19:02:58 +0530

    initial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Chat_Application_With_Nodejs_Application
+
+
+// Real time chat
+![alt text](image.png)
--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const http = require('http')
 const express = require('express')
 
-
 const app = express()
 const path = require('path');
 const { Socket } = require('dgram');
 const server = http.createServer(app);
-
 
 const { Server } = require("socket.io");
 const io = new Server(server)
@@ -15,7 +13,11 @@ app.use(express.static(path.resolve("./public")));
 
 //socket.io
 io.on('connection', (socket) =>{
-    console.log("A new user has connected", socket.id)
+   socket.on('user-message',(message)=>{
+    io.emit("message", message)
+
+
+   });
 })
 
 
